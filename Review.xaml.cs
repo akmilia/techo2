6a21f81fb096ef4e3390d99eb77e27d697371913
Zuuu <dzuu_n@mail.ru>
2024-06-09 11:18:53 +0300

    Скачай пакеты пж , Install-Package QRCoder Install-Package System.Drawing.Common

---
+++ Review.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace techo
+{
+    /// <summary>
+    /// Логика взаимодействия для Review.xaml
+    /// </summary>
+    public partial class Review : Window
+    {
+        public Review()
+        {
+            InitializeComponent(); LoadQR();
+        }
+        private void LoadQR()
+        {
+            string url = "https://docs.google.com/forms/d/e/1FAIpQLSdhZcExx6LSIXxk0ub55mSu-WIh23WYdGG9HY5EZhLDo7P8eA/viewform?usp=sf_link";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                qrCodeImage.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            QRCodeImage.Source = bitmapImage;
+        }
+    }
+}
